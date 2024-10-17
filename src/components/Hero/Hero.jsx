f93b0685994d88167e28c1d7f1adfa2fe5f582b1
Aphichat_Rungruang <aphichat.tkc@gmail.com>
2024-10-17 11:57:37 +0700

    update1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // ใช้ useNavigate
+import { useNavigate } from "react-router-dom";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
-  const navigate = useNavigate(); // ใช้ navigate
+  const navigate = useNavigate();
 
   const topics = [
-    { name: "Smart Solutions", imgSrc: "/assets/image/smartsolution.jpg" },
-    { name: "Cloud Solution", imgSrc: "/assets/image/cloudsolution.jpg" },
-    { name: "Cyber Security", imgSrc: "/assets/image/cybersecurity.jpg" },
-    { name: "Software Development", imgSrc: "/assets/image/softwaredevelopment.jpg" },
-    { name: "Green Solution", imgSrc: "/assets/image/greensolution.jpg" },
-    { name: "Telecom and Network Infrastructure", imgSrc: "/assets/image/Telecom-and-Network-Infrastructure.jpg" }, // เปลี่ยนชื่อไฟล์เพื่อหลีกเลี่ยงช่องว่าง
-    { name: "ICT Infrastructure", imgSrc: "/assets/image/ICTInfrastructure.jpg" },
-    { name: "Public Safety", imgSrc: "/assets/image/public-safety.jpg" }, // เปลี่ยนชื่อไฟล์เพื่อหลีกเลี่ยงช่องว่าง
-    { name: "Logistics", imgSrc: "/assets/image/Logistics.jpg" },
-    { name: "Edutech", imgSrc: "/assets/image/Edutech.jpg" },
-    { name: "Autonomous System", imgSrc: "/assets/image/Autonomous-System.jpg" } // เปลี่ยนชื่อไฟล์เพื่อหลีกเลี่ยงช่องว่าง
+    { name: "Smart Solutions", imgSrc: "assets/image/smartsolution.jpg" },
+    { name: "Cloud Solution", imgSrc: "assets/image/cloudsolution.jpg" },
+    { name: "Cyber Security", imgSrc: "assets/image/cybersecurity.jpg" },
+    { name: "Software Development", imgSrc: "assets/image/softwaredevelopment.jpg" },
+    { name: "Green Solution", imgSrc: "assets/image/greensolution.jpg" },
+    { name: "Telecom and Network Infrastructure", imgSrc: "assets/image/Telecom-and-Network-Infrastructure.jpg" },
+    { name: "ICT Infrastructure", imgSrc: "assets/image/ICTInfrastructure.jpg" },
+    { name: "Public Safety", imgSrc: "assets/image/public-safety.jpg" },
+    { name: "Logistics", imgSrc: "assets/image/Logistics.jpg" },
+    { name: "Edutech", imgSrc: "assets/image/Edutech.jpg" },
+    { name: "Autonomous System", imgSrc: "assets/image/Autonomous-System.jpg" }
   ];
+
   const handleButtonClick = (topic) => {
     if (topic.name === "Smart Solutions") {
-      navigate("/smart-solutions"); // นำทางไปยังหน้า SmartSolutions.jsx
+      navigate("/smart-solutions");
     }
   };
 
