cc6e18b6529738b213467f2fad5ebf45b8b2d01d
Aphichat_Rungruang <aphichat.tkc@gmail.com>
2024-10-17 09:10:33 +0700

    update2

--- page/SmartSolutions.jsx
+++ page/SmartSolutions.jsx
@@ -2,27 +2,63 @@ import React from "react";
 import styles from "./SmartSolutions.module.css";
 
 export const SmartSolutions = () => {
-  const buttons = [
+  const buttons1 = [
     { text: "AI", imgSrc: "/assets/image/AI.jpg" },
     { text: "Big Data", imgSrc: "/assets/image/bigdata.jpg" },
     { text: "Computer Vision", imgSrc: "/assets/image/computervision.jpg" },
+    // { text: "AR/VR/XR", imgSrc: "/assets/image/AR VR XR MR.jpg" },
+    // { text: "IOT", imgSrc: "/assets/image/IOT.jpg" },
+    // { text: "Machine-to-Machine Communication", imgSrc: "/assets/image/Machine-to-Machine.jpg" },
+    // { text: "mmWave", imgSrc: "/assets/image/MMwave.jpg" },
+
+  ];
+  const buttons2 = [
+    
     { text: "AR/VR/XR", imgSrc: "/assets/image/AR VR XR MR.jpg" },
     { text: "IOT", imgSrc: "/assets/image/IOT.jpg" },
     { text: "Machine-to-Machine Communication", imgSrc: "/assets/image/Machine-to-Machine.jpg" },
+  
+
+  ];
+  const buttons3 = [
+   
     { text: "mmWave", imgSrc: "/assets/image/MMwave.jpg" },
 
   ];
+
 
   return (
     <section className={styles.container}>
       <div className={styles.header}>Smart Solutions</div>
       <div className={styles.gridContainer}>
-        {buttons.map((button, index) => (
+        {buttons1.map((button, index) => (
           <button key={index}>
             <img src={button.imgSrc} alt={button.text} />
             <span>{button.text}</span>
           </button>
+          
         ))}
+        
+      </div>
+      <div className={styles.gridContainer}>
+        {buttons2.map((button, index) => (
+          <button key={index}>
+            <img src={button.imgSrc} alt={button.text} />
+            <span>{button.text}</span>
+          </button>
+          
+        ))}
+        
+      </div>
+      <div className={styles.gridContainer}>
+        {buttons3.map((button, index) => (
+          <button key={index}>
+            <img src={button.imgSrc} alt={button.text} />
+            <span>{button.text}</span>
+          </button>
+          
+        ))}
+        
       </div>
     </section>
   );
