edeb6eeed9ba1d5edb6de09d306e095ddcfc8530
Aphichat_Rungruang <aphichat.tkc@gmail.com>
2024-11-04 17:03:59 +0700

    updatehero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -42,23 +42,7 @@ export const Hero = () => {
   return (
     <>
       <Navbar />
-      <div className={styles.BG1}>
-        <div className={styles.topic1}>บริษัท เทิร์นคีย์ คอมมูนิเคชั่น เซอร์วิส จำกัด (มหาชน)</div>
-        {/* <div className={styles.topic1}>company in your industry.</div> */}
-        <div className={styles.topic2}>มีความยินดีเป็นอย่างยิ่งที่จะนำเสนอผลงานและบริการที่เราได้พัฒนาขึ้นเพื่อสนับสนุนความสำเร็จของลูกค้าและพันธมิตรทางธุรกิจ 
-                                       เรามุ่งมั่นในการนำเสนอโซลูชันที่ครบวงจรและตอบโจทย์การสื่อสารในยุคดิจิทัล ทั้งในด้านเทคโนโลยีการเชื่อมต่อ 
-                                       และการให้บริการด้วยมาตรฐานระดับสากล
-        </div>
-        <div className={styles.topic2}>ในทุกขั้นตอนของการทำงาน เราให้ความสำคัญกับนวัตกรรม คุณภาพ และความเชื่อมั่นที่ลูกค้าสามารถไว้วางใจได้ 
-        ด้วยทีมงานที่มีความเชี่ยวชาญและประสบการณ์ยาวนาน 
-        เรามุ่งสู่การเป็นผู้นำด้านการสื่อสารและไอซีทีครบวงจร เพื่อเสริมสร้างความแข็งแกร่งให้กับระบบโครงสร้างพื้นฐานการสื่อสารและไอซีทีของประเทศ
-        เราหวังว่าผลงานที่นำเสนอในครั้งนี้ จะเป็นส่วนหนึ่งในการสร้างความมั่นใจ และส่งเสริมการพัฒนาทางธุรกิจร่วมกันต่อไป</div>
-
-        
-        {/* <div className={styles.buttonContainer}>
-    <a  className={styles.primaryButtonTop}>Learn more</a>
-  </div> */}
-      </div>
+      
       
 
     
@@ -101,8 +85,7 @@ export const Hero = () => {
       <img src="/image/smartsolution2.png" className={styles.solutionImage} />
       <h3>Smart Building</h3>
       <br/>
-      <p>แนวทางหรือระบบที่ใช้เทคโนโลยีขั้นสูงและนวัตกรรมในการแก้ปัญหาหรือปรับปรุงกระบวนการต่างๆ เพื่อเพิ่มประสิทธิภาพ
-         ความสะดวกสบาย และประสบการณ์การใช้งาน โดยมุ่งเน้นการเชื่อมโยงข้อมูลและการทำงานร่วมกันระหว่างอุปกรณ์หรือระบบที่แตกต่างกัน</p>
+      <p></p>
       <div className={styles.buttonContainerOurservice}>
       {/* <a href="#learn-more" className={styles.primaryButtonTop}onClick={handleLearnMoreClick_smart_solutions} >Learn more</a> */}
         
@@ -112,7 +95,7 @@ export const Hero = () => {
     <div className={styles.solutionCard}>
       <img src="/image/cloudsolution.jpg"  className={styles.solutionImage} />
       <h3>Smart Hospital</h3>
-      <p>Protect your infrastructure with the latest in cybersecurity technology, ensuring your data stays safe.</p>
+      <p></p>
       <div className={styles.buttonContainerOurservice}>
         {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
       </div>
@@ -121,7 +104,7 @@ export const Hero = () => {
     <div className={styles.solutionCard}>
       <img src="/image/cybersecurity.jpg"  className={styles.solutionImage} />
       <h3>Smart Platform</h3>
-      <p>Transform your business with cloud solutions tailored to your unique needs.</p>
+      <p></p>
       <div className={styles.buttonContainerOurservice}>
         {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
       </div>
@@ -133,7 +116,7 @@ export const Hero = () => {
   <div className={styles.solutionCard}>
     <img src="/image/softwaredevelopment.jpg"  className={styles.solutionImage} />
     <h3>Smart Learning</h3>
-    <p>Unleash the power of artificial intelligence to make sense of your data and drive decision-making.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -148,7 +131,7 @@ export const Hero = () => {
    >
     <img src="/image/greensolution.png"  className={styles.solutionImage} />
     <h3>Smart Logistics</h3>
-    <p>Protect your infrastructure with the latest in cybersecurity technology, ensuring your data stays safe.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -156,7 +139,7 @@ export const Hero = () => {
   <div className={styles.solutionCard}>
     <img src="/image/Autonomous-System.jpg" className={styles.solutionImage} />
     <h3>Smart Organized Communication Cables</h3>
-    <p>Transform your business with cloud solutions tailored to your unique needs.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -168,7 +151,7 @@ export const Hero = () => {
   <div className={styles.solutionCard}>
     <img src="/image/Telecom-and-Network-Infrastructure.jpg"  className={styles.solutionImage} />
     <h3>Autonomous Solution</h3>
-    <p>Unleash the power of artificial intelligence to make sense of your data and drive decision-making.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -177,7 +160,7 @@ export const Hero = () => {
   <div className={styles.solutionCard}>
     <img src="/image/ICTInfrastructure.jpg"  className={styles.solutionImage} />
     <h3>Cyber Security</h3>
-    <p>Protect your infrastructure with the latest in cybersecurity technology, ensuring your data stays safe.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -185,7 +168,7 @@ export const Hero = () => {
   <div className={styles.solutionCard}>
     <img src="/image/public-safety.jpg" className={styles.solutionImage} />
     <h3>Smart Farming</h3>
-    <p>Transform your business with cloud solutions tailored to your unique needs.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -202,7 +185,7 @@ export const Hero = () => {
    >
     <img src="/image/Logistics.jpg"  className={styles.solutionImage} />
     <h3>Smart Utility(Grid)</h3>
-    <p>Unleash the power of artificial intelligence to make sense of your data and drive decision-making.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
@@ -211,7 +194,7 @@ export const Hero = () => {
   <div className={styles.solutionCard}>
     <img src="/image/Edutech.jpg"  className={styles.solutionImage} />
     <h3>Cloud Service</h3>
-    <p>Protect your infrastructure with the latest in cybersecurity technology, ensuring your data stays safe.</p>
+    <p></p>
     <div className={styles.buttonContainerOurservice}>
     {/* <a href="#learn-more" className={styles.primaryButtonTop}>Learn more</a> */}
     </div>
